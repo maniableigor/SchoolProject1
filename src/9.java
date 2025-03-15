12bf3390fb20789b2ac974b88d902ce8e7eec43b
maniableigor <maniableigor@gmail.com>
2025-03-15 06:01:47 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class SchoolProject1 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int num = rand.nextInt(10);
+        System.out.println("Random number: " + num);
+    }
+}
