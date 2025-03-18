d5595291cd2c57ae38fc96cb676455828b629095
maniableigor <maniableigor@gmail.com>
2025-03-18 17:35:30 +0000

    Add 12.java

---
+++ src/12.java
@@ -0,0 +1,11 @@
+public class MyClass {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 10;
+    if (x > y) {
+      System.out.println("x is greater than y");
+    } else {
+      System.out.println("y is greater than or equal to x");
+    }
+  }
+}
