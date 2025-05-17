1c1c1360655577a585a56a51436a9f8cdbbfeb06
maniableigor <maniableigor@gmail.com>
2025-05-17 03:13:40 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,8 @@
+// This is a simple Java program to demonstrate some basic GitHub workflow concepts.
+// Please replace this with your own code.
+
+// Step 1: Initialize Git and add new files
+
+git init
+git add .
+git commit -m "Initial commit"
